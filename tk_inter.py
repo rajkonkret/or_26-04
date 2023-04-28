60eb34b2fc7c0d7d8d9994eda2a51be83b255a13
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-04-28 15:57:44 +0200

    add tkinter

---
+++ tk_inter.py
@@ -0,0 +1,20 @@
+import tkinter
+
+
+class MyGui:
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+
+        self.label1 = tkinter.Label(self.main_window, text="Witaj świecie")
+        self.label2 = tkinter.Label(self.main_window, text="To jest program z GUI")
+        self.label3 = tkinter.Label(self.main_window, text="Test z prawej")
+        self.label4 = tkinter.Label(self.main_window, text="Koniec")
+
+        self.label1.pack(side='left')
+        self.label2.pack(side='bottom')
+        self.label3.pack(side='right')
+        self.label4.pack(side='top')
+        tkinter.mainloop()
+
+
+my_gui = MyGui()
